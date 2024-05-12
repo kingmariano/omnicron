79700476edcc39f6e164bc26ecbdcd780f8c8387
Charles Ozochukwu <charlesozochukwu2004@gmail.com>
2024-05-12 12:16:32 +0100

    created  github/workflows

--- python-server/main.py
+++ python-server/main.py
@@ -1,13 +1,14 @@
-import asyncio
+# import asyncio
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from endpoints import router
+import uvicorn
 
-loop = asyncio.get_event_loop()
-print(f"Using event loop: {type(loop)}\n")
-print(f"Current event loop policy: {asyncio.get_event_loop_policy()}")
+# loop = asyncio.get_event_loop()
+# print(f"Using event loop: {type(loop)}\n")
+# print(f"Current event loop policy: {asyncio.get_event_loop_policy()}")
 
-app = FastAPI(title="omnicron backend server")
+app = FastAPI(title="omnicron python backend server")
 
 app.add_middleware(
     CORSMiddleware,
@@ -21,5 +22,5 @@ app.include_router(router, prefix="/api/v1")
 def read_root():
     return {"Hello": "World"}
 
-
-
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
