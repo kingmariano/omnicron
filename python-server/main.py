35f3f23cdbbecca6459f18e5c976326a504ee0f6
Charles Ozochukwu <charlesozochukwu2004@gmail.com>
2024-05-12 16:37:44 +0100

    updated code

--- python-server/main.py
+++ python-server/main.py
@@ -23,4 +23,4 @@ def read_root():
     return {"Hi": "World"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
