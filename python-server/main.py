9a8f6627c60b8cafe2f90dd54035067718a30195
Charles Ozochukwu <charlesozochukwu2004@gmail.com>
2024-05-12 12:24:45 +0100

    updated main.py

--- python-server/main.py
+++ python-server/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
 app.include_router(router, prefix="/api/v1")
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hi": "World"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
