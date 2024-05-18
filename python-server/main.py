5158cd0f9f5b11e2b1650354169139749543f593
Charles Ozochukwu <charlesozochukwu2004@gmail.com>
2024-05-18 23:53:44 +0100

    changed port value to 8000

--- python-server/main.py
+++ python-server/main.py
@@ -28,4 +28,4 @@ def read_root():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
