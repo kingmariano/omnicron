f9b65ecb502085d129bff0324512a42318b08217
kingmariano <charlesozochukwu2004@gmail.com>
2024-06-28 20:06:50 +0100

    updated test cases

--- tests/router_test.go
+++ tests/router_test.go
@@ -3,7 +3,7 @@ package tests
 import (
 	"net/http"
 	"net/http/httptest"
-	// "os"
+	"os"
 	"testing"
 
 	"github.com/go-chi/chi/v5"
@@ -24,18 +24,18 @@ import (
 )
 
 func setupRouter(t *testing.T) (*chi.Mux, *config.ApiConfig) {
-	apiKey, grokApiKey, replicateApiKey, cloudinaryURL, port, youtubeDeveloperKey, err := utils.LoadEnv("../.env")
+	// apiKey, grokApiKey, replicateApiKey, cloudinaryURL, port, youtubeDeveloperKey, err := utils.LoadEnv("../.env")
 
-	if err != nil {
-		t.Fatal(err)
-	}
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
 
-	// apiKey := os.Getenv("API_KEY")
-	// grokApiKey := os.Getenv("GROK_API_KEY")
-	// port := os.Getenv("PORT")
-	// replicateApiKey := os.Getenv("REPLICATE_API_TOKEN")
-	// cloudinaryURL := os.Getenv("CLOUDINARY_URL")
-	// youtubeDeveloperKey := os.Getenv("YOUTUBE_DEVELOPER_KEY")
+	apiKey := os.Getenv("API_KEY")
+	grokApiKey := os.Getenv("GROK_API_KEY")
+	port := os.Getenv("PORT")
+	replicateApiKey := os.Getenv("REPLICATE_API_TOKEN")
+	cloudinaryURL := os.Getenv("CLOUDINARY_URL")
+	youtubeDeveloperKey := os.Getenv("YOUTUBE_DEVELOPER_KEY")
 
 	if apiKey == "" || grokApiKey == "" || replicateApiKey == "" || cloudinaryURL == "" || port == "" || youtubeDeveloperKey == "" {
 		t.Fatal("unable to get API key or port from environment variables")
